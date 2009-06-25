6d0656b3b111f027ced38a1dcc1d90a8bb50416f
Maarten Billemont <lhunath@gmail.com>
2009-06-25 23:28:05 +0200

    Migrate to the Java Research License

--- Classes/ThrowController.h
+++ Classes/ThrowController.h
@@ -1,3 +1,19 @@
+/*
+ * This file is part of Gorillas.
+ *
+ *  Gorillas is open software: you can use or modify it under the
+ *  terms of the Java Research License or optionally a more
+ *  permissive Commercial License.
+ *
+ *  Gorillas is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  You should have received a copy of the Java Research License
+ *  along with Gorillas in the file named 'COPYING'.
+ *  If not, see <http://stuff.lhunath.com/COPYING>.
+ */
+
 //
 //  ThrowController.h
 //  Gorillas
